a10211d6a0d8c892c064afe2bc13badfbcff3241
Aleksandra Kucharska <aleksandra0201@gmail.com>
2020-07-22 18:53:06 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const compareGuesses = (currentHumanGuess, computerGuess, target) => {
+    if (Math.abs(currentHumanGuess - target) < Math.abs(computerGuess - target)) {
+        return true;
+    } else if (Math.abs(computerGuess - target) < Math.abs(currentHumanGuess - target)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    } else {
+        return humanScore += 1;
+    }
+};
+
+const advanceRound = () => {
+    currentRoundNumber += 1;
+}
